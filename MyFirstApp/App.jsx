cf4494b1c38ef3b02659683f8b3145f4f499d4ee
mosmee21 <mousamiben.desai@edu.sait.ca>
2023-10-24 15:01:45 -0600

    changes made

--- MyFirstApp/App.jsx
+++ MyFirstApp/App.jsx
@@ -4,7 +4,7 @@
  * @format
  */
 
-import React from 'react';
+import React , { useState }from 'react';
 import {
   SafeAreaView,
   StyleSheet,
@@ -19,9 +19,16 @@ import ToDoForm from './src/ToDoForm';
 
 
 function App() {
+  const {tasks, setTasks} = useState([
+    'Do laundry',
+    'Go to gym',
+    'Walk dog',
+    'Study'
+  ]);
+
   return (
     <SafeAreaView>
-     <ToDoForm/>
+     <ToDoList  tasks = {tasks}/>
      <ToDoForm/>
       
     </SafeAreaView>
--- MyFirstApp/src/ToDoList.jsx
+++ MyFirstApp/src/ToDoList.jsx
@@ -1,16 +1,27 @@
 import React from 'react';
-import {StyleSheet} from 'react-native';
+import { StyleSheet } from 'react-native';
 import {
-    Pressable,
-    View,
-    Text,
-    ScrollView  } from 'react-native';
+  Pressable,
+  View,
+  Text,
+  ScrollView
+} from 'react-native';
 
-function ToDoList() {
-    return (
-        <>
-         <ScrollView>
-        <Pressable>
+function ToDoList({ tasks = [] }) {
+  return (
+    <>
+      <ScrollView>
+        {tasks.map((task, index) => (
+          <>
+            <Pressable key={index} >
+              <View style={[styles.task, styles.completed]}>
+                <Text style={styles.taskText}>{task}</Text>
+              </View>
+            </Pressable>
+          </>
+
+        ))}
+        {/*<Pressable>
           <View style={[styles.task, styles.completed]}>
             <Text style={styles.taskText}>Do laundry</Text>
           </View>
@@ -24,24 +35,24 @@ function ToDoList() {
           <View style={[styles.task, styles.completed]}>
             <Text style={styles.taskText}>Walk dog</Text>
           </View>
-        </Pressable>
+        </Pressable>*/}
       </ScrollView>
-        </>
-    );
+    </>
+  );
 }
 
 const styles = StyleSheet.create({
-    task: {
-      padding: 10,
-      borderBottomWidth: 1,
-      borderColor: '#ccc',
-    },
-    completed: {
-      backgroundColor: '#e0e0e0',
-    },
-    taskText: {
-        fontSize: 16,
-      },
+  task: {
+    padding: 10,
+    borderBottomWidth: 1,
+    borderColor: '#ccc',
+  },
+  completed: {
+    backgroundColor: '#e0e0e0',
+  },
+  taskText: {
+    fontSize: 16,
+  },
 });
 
 export default ToDoList;
