73e85197808167d51787865f5168cd21a4c839b9
mosmee21 <mousamiben.desai@edu.sait.ca>
2023-11-22 16:25:00 -0700

    changes made

--- MyFirstApp/src/screens/Home.jsx
+++ MyFirstApp/src/screens/Home.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { SafeAreaView , Button} from 'react-native';
 
-import ToDoList from '../ToDoList';
-import ToDoForm from '../ToDoForm';
+import ToDoList from '..Components/ToDoList';
+import ToDoForm from '..Components/ToDoForm';
 
 function Home({navigation}) {
     const [tasks, setTasks] = React.useState([
