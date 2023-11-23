036489e0eda9587e63dfcf6e7a9bde2e481f84fe
mosmee21 <mousamiben.desai@edu.sait.ca>
2023-11-22 17:04:38 -0700

    changes to app.jsx

---
+++ MyFirstApp/src/App.jsx
@@ -0,0 +1,35 @@
+/**
+ * My To Do List App
+ *
+ * @format
+ */
+
+import React from 'react';
+import {StyleSheet} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import HomeScreen from './src/screens/Home';
+import AboutScreen from './src/screens/About';
+
+
+const Stack = createStackNavigator();
+
+
+function App() {
+  return (
+    <NavigationContainer>
+       <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="About" component={AboutScreen} />
+      </Stack.Navigator>
+      
+    </NavigationContainer>  
+  );
+}
+
+const styles = StyleSheet.create({
+ 
+});
+
+export default App;
